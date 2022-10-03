b27ab922c3d7277ad008a06ef79e487c07ea7aa9
TkachukSasha <sashatkachuk09@ukr.net>
2022-10-03 14:32:14 +0300

    feat 1: deleted some unnecessary code

--- QueryConverter.Backend/src/Presentation/QueryConverter.UI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace QueryConverter.UI.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
