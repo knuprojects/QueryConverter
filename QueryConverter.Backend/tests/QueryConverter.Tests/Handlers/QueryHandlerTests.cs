5fae38c8ce870437677176c157055f2bfdcca858
TkachukSasha <sashatkachuk09@ukr.net>
2022-10-04 16:17:39 +0300

    feat 4: added test with exception

--- QueryConverter.Backend/tests/QueryConverter.Tests/Handlers/QueryHandlerTests.cs
+++ QueryConverter.Backend/tests/QueryConverter.Tests/Handlers/QueryHandlerTests.cs
@@ -28,5 +28,44 @@ namespace QueryConverter.Tests.Handlers
             // Assert
             Assert.NotNull(result);
         }
+
+        [Fact]
+        public async Task HandleSelectStatement_Should_Be_Exception()
+        {
+            // Arrange
+            var statement = ExtensionMethods.ConvertToTSQL(QueryTemplate.InvalidSelectWithFilter);
+
+            // Act
+            _queryHandler.HandleSelectStatement(statement);
+
+            // Assert
+            Assert.Null(null);
+        }
+
+        [Fact]
+        public async Task HandleGroupByStatement_Should_Return_Valid_Query()
+        {
+            // Arrange
+            var statement = ExtensionMethods.ConvertToTSQL(QueryTemplate.SelectWithFilterAndGroupBy);
+
+            // Act
+            var result = _queryHandler.HandleGroupByStatement(statement);
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task HandleGroupByStatement_Should_Be_Exception()
+        {
+            // Arrange
+            var statement = ExtensionMethods.ConvertToTSQL(QueryTemplate.InvalidSelectWithFilterAndGroupBy);
+
+            // Act
+            _queryHandler.HandleGroupByStatement(statement);
+
+            // Assert
+            Assert.Null(null);
+        }
     }
 }
