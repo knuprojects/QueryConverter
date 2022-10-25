8057eb008f1a81ff62c3f2f09c92d5af1cafed93
TkachukSasha <sashatkachuk09@ukr.net>
2022-10-25 21:41:36 +0300

    feat: added cqrs base structure

---
+++ QueryConverter.Backend/src/Presentation/QueryConverter.Presentation/Infrastructure/CqrsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using QueryConverter.Shared.Cqrs.Queries;
+
+namespace QueryConverter.Presentation.Infrastructure
+{
+    public static class CqrsExtensions
+    {
+        public static IServiceCollection AddCqrs(this IServiceCollection services)
+            => services
+                     .AddQueries();
+
+        public static IServiceCollection AddQueries(this IServiceCollection services)
+        {
+            services.Scan(s => s.FromAssemblies(AppDomain.CurrentDomain.GetAssemblies())
+                    .AddClasses(c => c.AssignableTo(typeof(IQueryHandler<,>)))
+                    .AsImplementedInterfaces()
+                    .WithScopedLifetime());
+
+            return services;
+        }
+    }
+}
--- QueryConverter.Backend/src/Shared/QueryConverter.Shared/Utils/Extensions/ExtensionMethods.cs
+++ QueryConverter.Backend/src/Shared/QueryConverter.Shared/Utils/Extensions/ExtensionMethods.cs
@@ -6,7 +6,7 @@ public static class ExtensionMethods
 {
     public static string PrettyJson(this string jsonString)
     {
-        dynamic parsedJson = JsonConvert.DeserializeObject(jsonString);
+        var parsedJson = JsonConvert.DeserializeObject(jsonString);
         return JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
     }
 
