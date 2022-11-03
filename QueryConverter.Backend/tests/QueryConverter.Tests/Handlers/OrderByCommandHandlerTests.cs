a286bc2d6b3466e65deb9c5d4bf771feb0857122
Illia-creator <sie29092002@gmail.com>
2022-11-03 21:56:51 +0200

    feat: add order by test

---
+++ QueryConverter.Backend/tests/QueryConverter.Tests/Handlers/OrderByCommandHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿using QueryConverter.Core.Handlers.Commands;
+using QueryConverter.Core.Handlers;
+using QueryConverter.Shared.Cqrs.Commands;
+using QueryConverter.Types.Shared.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QueryConverter.Core.Utils;
+using QueryConverter.Tests.Templates;
+
+namespace QueryConverter.Tests.Handlers
+{
+    public class OrderByCommandHandlerTests
+    {
+        private readonly ICommandHandler<OrderByCommand, ResultModel> _orderByCommandHandler;
+        private readonly ICondition _condition;
+
+        public OrderByCommandHandlerTests(ICondition condition)
+        {
+            _condition = condition;
+            _orderByCommandHandler = new OrderByCommandHandler(_condition);
+        }
+
+        [Fact]
+        public async Task HandleOrderByStatement_Should_Return_Valid_Query()
+        {
+            var command = new OrderByCommand()
+            {
+                SQLQuery = QueryTemplate.SelectWithOrderBy
+            };
+
+            var result = await _orderByCommandHandler.HandleAsync(command);
+
+            result.Should().NotBeNull();
+        }       
+    }
+}
